8a7b227256fbdbc6d0aa251e2e2e839c93d08691
Erick Rodriguez <erickrod54@gmail.com>
2022-08-24 02:47:38 -0400

    serveless-with-react-practice version 1 - App js file - Features: Importing and placing Basic Component, Importing and placing Airtable Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import Basic from "./Basic";
+import Airtable from "./Airtable";
+/** Serveless-with-react-practice version 1 - App js
+ * file - Features: 
+ * 
+ *        --> Importing and placing 'Basic' 
+ *            Component.
+ * 
+ *        --> Importing and placing 'Airtable'
+ *            Component.
+ * 
+ * Note: These Components handle fetching API's the
+ * following ways:
+ * 
+ *      --> 'Basic' Component ----> fetching an
+ *            existing API just having the url-
+ * 
+ *      --> 'Airtable' Component--> i create a 
+ *           back-end with serveless functions
+ *           and run the API in my own local 
+ *           server
+ */
+
 function App() {
-  return <h2>React and Serverless Functions</h2>
+  return <>
+    <Basic />
+    <Airtable />
+  </>
 }
 
 export default App
