95d3e4e6d5d0abe8c6c50f7eba5bf06b0d90865e
Erick Rodriguez <erickrod54@gmail.com>
2022-08-25 00:19:44 -0400

    serveless-with-react-practice app version 2 - Basic js file - Features: Fetching the data from the API url -this way i test that i can fetch an existing API just having the url-, Building the return for the front-end

--- src/Basic.js
+++ src/Basic.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 const url = 'https://serveless-functions-practice.netlify.app/api/2-basic-api'
 
-/**serveless-with-react-practice app version 1 - Basic js file 
+/**serveless-with-react-practice app version 2 - Basic js file 
  * - Features:
  * 
  *      --> Fetching the data from the API 'url'
+ *          - this way i test that i can fetch an
+ *            existing API jus having the url-
+ * 
+ *      --> Building the return for the front-end.   
  * 
  * Note: this api 'url' was previusly built for the 
  * serveless-functions-practice, this practice is
@@ -20,7 +24,7 @@ const Basic = () => {
     const fetchData = async () => {
         try {
             const AxiosData  = await axios.get(url)
-            console.log('the axios data ==>', AxiosData)
+            //console.log('the axios data ==>', AxiosData)
             const { data } = await axios.get(url)
             setProducts(data)
         } catch (error) {
@@ -33,11 +37,31 @@ const Basic = () => {
         fetchData()
     }, [])
 
-    console.log('this is what i get from the API', products)
+    //console.log('this is what i get from the API', products)
 
+    /**here i set the styles and build the return 
+     * for the data previusly fetched */
     return(
-        <section>
-            hello from basic example
+        <section className="section section-center">
+            <div className="title">
+                <h2>basic setup</h2>
+                <div className="title-underline"/>
+            </div>
+            <div className="products">
+                {products.map((product) => {
+                    const {id, image:{url}, price, name} = product;
+                    
+                    return(
+                        <article className="product" key={id}>
+                            <img src={url} alt={name}/>
+                            <div className="info">
+                                <h5>{name}</h5>
+                                <h5 className="price">${price}</h5>
+                            </div>
+                        </article>
+                        )
+                })}
+            </div>
         </section>
     )
 }
