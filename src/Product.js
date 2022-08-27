42fe8a429963f89513ad4115c95493c866fe8600
Erick Rodriguez <erickrod54@gmail.com>
2022-08-26 20:24:13 -0400

    serveless-with-react-practice app version 4 - Product Component - Features: Building a basic Product Component to test the routing

---
+++ src/Product.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+/**serveless-with-react-practice app version 4 - Product
+ * Component - Features:
+ * 
+ *      --> Building a basic 'Product' Component
+ *         to test the  routing   
+ * 
+ * Note: After setting it up the routing the message of 
+ * this Component should be displayed once i click on the 
+ * 'Airtable' > 'Product' picture   
+ */
+
+const Product = () => {
+
+    return(
+        <h2>single product page</h2>
+    )
+}
+
+export default Product;
