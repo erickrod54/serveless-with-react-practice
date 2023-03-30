84bde94ea6ee458fa80189275212ed9f7853fd61
Erick Rodriguez <erickrod54@gmail.com>
2023-03-30 19:57:24 -0400

    Serveless-with-react app version 6.02 - data js - Features: Building data js file, Placing first data url

---
+++ src/data.js
@@ -0,0 +1,13 @@
+/**serveless-with-react-practice app version 6.02 - data js
+ * - Features:
+ * 
+ *      --> Building data js file.
+ * 
+ *      --> Placing frist data 'url'   
+ * 
+ * Note: this api 'url' was previusly built for the 
+ * serveless-functions-practice, this practice is
+ * in order to use it with a react front-end.
+ */
+
+export const url = 'https://serveless-functions-practice.netlify.app/api/2-basic-api';
